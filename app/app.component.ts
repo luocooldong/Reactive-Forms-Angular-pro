77da484e6b748782966a20e6432fcd2e543f40bf
gengyudonga@gmail.com <gengyudonga@gmail.com>
2017-08-29 20:46:46 +0800

    Using ngTemplateOutlet with context

--- app/app.component.ts
+++ app/app.component.ts
@@ -15,17 +15,20 @@ import { User } from './auth-form/auth-form.interface';
 @Component({
   selector: 'app-root',
   template: `
-        <div>
-          <ng-container
-            [ngTemplateOutlet]="tmpl">
-          </ng-container>
-          <template #tmpl>
-            Todd Motto : England, UK
-          </template>
-        </div>
+    <div>
+        <ng-container
+          [ngTemplateOutlet]="tmpl"
+          [ngTemplateOutletContext]="ctx">
+        </ng-container>
+        <template #tmpl let-name let-location="location">
+          {{ name }} : {{ location }}
+        </template>
+    </div>
   `
 })
 export class AppComponent {
-
-
+  ctx = {
+    $implicit: 'Todd Motto',
+    location: 'England, UK'
+  };
 }
