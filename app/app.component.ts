b12800c9141590d166fbed56cc6210831e43af57
gengyudonga@gmail.com <gengyudonga@gmail.com>
2017-08-29 20:39:29 +0800

    Dynamic template rendering with ngTemplateOutlet

--- app/app.component.ts
+++ app/app.component.ts
@@ -16,35 +16,16 @@ import { User } from './auth-form/auth-form.interface';
   selector: 'app-root',
   template: `
         <div>
-
-           <div #entry></div>
-           <template #tmpl let-name let-location="location">
-             {{name}} : {{ location }}
-           </template>
-
+          <ng-container
+            [ngTemplateOutlet]="tmpl">
+          </ng-container>
+          <template #tmpl>
+            Todd Motto : England, UK
+          </template>
         </div>
   `
 })
-export class AppComponent implements AfterContentInit {
+export class AppComponent {
 
-      component: ComponentRef<AuthFormComponent>;
-
-      @ViewChild('entry', { read: ViewContainerRef }) entry: ViewContainerRef;
-
-      @ViewChild('tmpl') tmpl: TemplateRef<any>;
-
-      constructor(private resolver: ComponentFactoryResolver) {
-
-
-      }
-
-      ngAfterContentInit() {
-        //Called after ngOnInit when the component's or directive's content has been initialized.
-        //Add 'implements AfterContentInit' to the class.
-        this.entry.createEmbeddedView(this.tmpl, {
-          $implicit: 'Yudong',
-          location: 'UK, England'
-        });
-      }
 
 }
