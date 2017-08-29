cb1b386d1c4ba09f4ebf31b38516e433e3a55779
gengyudonga@gmail.com <gengyudonga@gmail.com>
2017-08-29 19:07:04 +0800

    Dynamic components with ComponentFactoryResolver

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,39 +1,45 @@
-import { Component } from '@angular/core';
+import { resolve } from 'path';
+import { privateEncrypt } from 'crypto';
+import { AfterContentInit, Component, ComponentFactoryResolver, ViewChild, ViewContainerRef } from '@angular/core';
+
+import { AuthFormComponent } from './auth-form/auth-form.component';
 
 import { User } from './auth-form/auth-form.interface';
 
 @Component({
   selector: 'app-root',
   template: `
-    <div>
-      <auth-form 
-        (submitted)="loginUser($event)">
-        <h3>Login</h3>
-        <auth-remember (checked)="rememberUser($event)"></auth-remember>
-        <auth-remember (checked)="rememberUser($event)"></auth-remember>
-        <button type="submit">
-             Login
-        </button>
-      </auth-form>
-
-      
-    </div>
+  <div>
+  <div #entry></div>
+</div>
   `
 })
-export class AppComponent {
+export class AppComponent implements AfterContentInit {
+  
+  @ViewChild('entry', {read: ViewContainerRef }) entry: ViewContainerRef;
 
-  rememberMe: boolean = false;
+  constructor(private  resolver: ComponentFactoryResolver ){
+    
 
-  rememberUser(remember: boolean){
-    this.rememberMe = remember;
   }
+
+  ngAfterContentInit() {
+    //Called after ngOnInit when the component's or directive's content has been initialized.
+    //Add 'implements AfterContentInit' to the class.
+    const auhtFormFactory = this.resolver.resolveComponentFactory(AuthFormComponent);
+    const component = this.entry.createComponent(auhtFormFactory);
+
+
+    
+  }
+
 
   createUser(user: User) {
     console.log('Create account', user);
   }
 
   loginUser(user: User) {
-    console.log('Login', user, this.rememberMe);
+    console.log('Login', user,);
   }
 
 }
