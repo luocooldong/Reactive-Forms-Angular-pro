91d938228485c34ddfdd55d2ee01c6caea920eb3
gengyudonga@gmail.com <gengyudonga@gmail.com>
2017-08-29 20:08:50 +0800

    Dynamic components reordering

--- app/app.component.ts
+++ app/app.component.ts
@@ -10,45 +10,52 @@ import { User } from './auth-form/auth-form.interface';
   template: `
         <div>
            <button (click)="destroyComponent()" > Destroy </button>
+           <button (click)="moveComponent()" >  Move </button>
            <div #entry></div>
         </div>
   `
 })
 export class AppComponent implements AfterContentInit {
 
-  component: ComponentRef<AuthFormComponent>;
-  
-  @ViewChild('entry', {read: ViewContainerRef }) entry: ViewContainerRef;
+      component: ComponentRef<AuthFormComponent>;
 
-  constructor(private  resolver: ComponentFactoryResolver ){
-    
+      @ViewChild('entry', { read: ViewContainerRef }) entry: ViewContainerRef;
 
-  }
-
-  ngAfterContentInit() {
-    //Called after ngOnInit when the component's or directive's content has been initialized.
-    //Add 'implements AfterContentInit' to the class.
-    const auhtFormFactory = this.resolver.resolveComponentFactory(AuthFormComponent);
-    this.component = this.entry.createComponent(auhtFormFactory);
-    console.log(this.component.instance);
-
-    this.component.instance.title = 'Create account';
-    this.component.instance.submitted.subscribe(this.loginUser);
-  }
-
-  destroyComponent(){
-    console.log(this.component);
-    this.component.destroy();
-  }
+      constructor(private resolver: ComponentFactoryResolver) {
 
 
-  createUser(user: User) {
-    console.log('Create account', user);
+      }
 
-  }
+      ngAfterContentInit() {
+        //Called after ngOnInit when the component's or directive's content has been initialized.
+        //Add 'implements AfterContentInit' to the class.
+        const auhtFormFactory = this.resolver.resolveComponentFactory(AuthFormComponent);
+        this.entry.createComponent(auhtFormFactory);
 
-  loginUser(user: User) {
-    console.log('Login', user);
-  }
+        this.component = this.entry.createComponent(auhtFormFactory, 0);
+        console.log(this.component.instance);
+
+        this.component.instance.title = 'Create account';
+        this.component.instance.submitted.subscribe(this.loginUser);
+      }
+
+      destroyComponent() {
+        console.log(this.component);
+        this.component.destroy();
+      }
+
+      moveComponent(){
+        this.entry.move(this.component.hostView, 1);
+      }
+
+
+      createUser(user: User) {
+        console.log('Create account', user);
+
+      }
+
+      loginUser(user: User) {
+        console.log('Login', user);
+      }
 
 }
