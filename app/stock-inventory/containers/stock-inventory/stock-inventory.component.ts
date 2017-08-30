f4ebc2d80c05b525eaca171b38383e67517e1d13
gengyudonga@gmail.com <gengyudonga@gmail.com>
2017-08-30 19:19:44 +0800

    Binding FormControls to select

--- app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
+++ app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, FormArray } from '@angular/forms';
 
+import { Product } from '../../models/product.interface';
+
 @Component({
   selector: 'stock-inventory',
   styles: ['stock-inventory.component.scss'],
@@ -13,7 +15,8 @@ import { FormControl, FormGroup, FormArray } from '@angular/forms';
         </stock-branch>
 
         <stock-selector
-          [parent]="form">
+          [parent]="form"
+          [products]="products">
         </stock-selector>
 
         <stock-products
@@ -35,6 +38,15 @@ import { FormControl, FormGroup, FormArray } from '@angular/forms';
   `
 })
 export class StockInventoryComponent {
+
+  products: Product[] = [
+    { "id": 1, "price": 2800, "name": "MacBook Pro" },
+    { "id": 2, "price": 50, "name": "USB-C Adaptor" },
+    { "id": 3, "price": 400, "name": "iPod" },
+    { "id": 4, "price": 900, "name": "iPhone" },
+    { "id": 5, "price": 600, "name": "Apple Watch" },
+  ];
+
   form = new FormGroup({
     store: new FormGroup({
       branch: new FormControl(''),
