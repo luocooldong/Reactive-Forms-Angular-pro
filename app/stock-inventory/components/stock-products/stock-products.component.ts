db238bbac1410b8e2d647a9eeebcfdefc1e1d18a
gengyudonga@gmail.com <gengyudonga@gmail.com>
2017-08-30 23:02:39 +0800

    Http service and joining Observables

--- app/stock-inventory/components/stock-products/stock-products.component.ts
+++ app/stock-inventory/components/stock-products/stock-products.component.ts
@@ -1,18 +1,23 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { FormGroup, FormArray } from '@angular/forms';
 
+import { Product } from '../../models/product.interface';
+
 @Component({
   selector: 'stock-products',
   styleUrls: ['stock-products.component.scss'],
   template: `
-  <div class="stock-product" [formGroup]="parent">
+    <div class="stock-product" [formGroup]="parent">
       <div formArrayName="stock">
         <div
           *ngFor="let item of stocks; let i = index;">
           
           <div class="stock-product__content" [formGroupName]="i">
             <div class="stock-product__name">
-              {{ item.value.product_id }}
+              {{ getProduct(item.value.product_id).name }}
+            </div>
+            <div class="stock-product__price">
+              {{ getProduct(item.value.product_id).price | currency:'USD':true }}
             </div>
             <input 
               type="number"
@@ -21,30 +26,37 @@ import { FormGroup, FormArray } from '@angular/forms';
               max="1000"
               formControlName="quantity">
             <button 
-               type="button"
-               (click)="onRemove(item, i)"  >
+              type="button"
+              (click)="onRemove(item, i)">
               Remove
             </button>
           </div>
 
         </div>
       </div>
-  </div>
+    </div>
   `
 })
 export class StockProductsComponent {
   @Input()
   parent: FormGroup;
 
+  @Input()
+  map: Map<number, Product>;
+
   @Output()
   removed = new EventEmitter<any>();
 
+  getProduct(id) {
+    return this.map.get(id);
+  }
+
+  onRemove(group, index) {
+    this.removed.emit({ group, index });
+  }
+
   get stocks() {
     return (this.parent.get('stock') as FormArray).controls;
-  }
-  
-  onRemove(group, index ){
-    this.removed.emit({ group, index });
   }
 
 }
--- app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
+++ app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, FormArray } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
 
-import { Product } from '../../models/product.interface';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/forkJoin';
+
+import { StockInventoryService } from '../../services/stock-inventory.service';
+
+import { Product, Item } from '../../models/product.interface';
 
 @Component({
   selector: 'stock-inventory',
-  styles: ['stock-inventory.component.scss'],
+  styleUrls: ['stock-inventory.component.scss'],
   template: `
     <div class="stock-inventory">
       <form [formGroup]="form" (ngSubmit)="onSubmit()">
@@ -17,12 +22,13 @@ import { Product } from '../../models/product.interface';
         <stock-selector
           [parent]="form"
           [products]="products"
-          (added)="addStack($event)" >
+          (added)="addStock($event)">
         </stock-selector>
 
         <stock-products
           [parent]="form"
-          (removed)="removeStock($event)" >
+          [map]="productMap"
+          (removed)="removeStock($event)">
         </stock-products>
 
         <div class="stock-inventory__buttons">
@@ -39,57 +45,59 @@ import { Product } from '../../models/product.interface';
     </div>
   `
 })
-export class StockInventoryComponent {
+export class StockInventoryComponent implements OnInit {
 
-  products: Product[] = [
-    { "id": 1, "price": 2800, "name": "MacBook Pro" },
-    { "id": 2, "price": 50, "name": "USB-C Adaptor" },
-    { "id": 3, "price": 400, "name": "iPod" },
-    { "id": 4, "price": 900, "name": "iPhone" },
-    { "id": 5, "price": 600, "name": "Apple Watch" },
-  ];
+  products: Product[];
+
+  productMap: Map<number, Product>;
 
   form = this.fb.group({
     store: this.fb.group({
       branch: '',
       code: ''
     }),
-    selector: new FormGroup({
-      product_id: new FormControl(''),
-      quantity: new FormControl(10)
-    }),
-    stock: this.fb.array([
-      this.createStock({ product_id: 1, quantity: 10 }),
-      this.createStock({ product_id: 3, quantity: 50 }),
-      this.createStock({ product_id: 5, quantity: 100 }),
-    ])
+    selector: this.createStock({}),
+    stock: this.fb.array([])
   })
 
-  constructor(private fb: FormBuilder){
+  constructor(
+    private fb: FormBuilder,
+    private stockService: StockInventoryService
+  ) {}
+
+  ngOnInit() {
+    const cart = this.stockService.getCartItems();
+    const products = this.stockService.getProducts();
+
+    Observable
+      .forkJoin(cart, products)
+      .subscribe(([cart, products]: [Item[], Product[]]) => {
+        
+        const myMap = products
+          .map<[number, Product]>(product => [product.id, product]);
+        
+        console.log(myMap);
+
+        this.productMap = new Map<number, Product>(myMap);
+        this.products = products;
+        cart.forEach(item => this.addStock(item));
+      });
 
   }
 
-
-
-
-
-
-
   createStock(stock) {
-    return new FormGroup({
-      product_id: new FormControl(parseInt(stock.product_id, 10) || ''),
-      quantity: new FormControl(stock.quantity || 10)
+    return this.fb.group({
+      product_id: parseInt(stock.product_id, 10) || '',
+      quantity: stock.quantity || 10
     });
   }
 
-  addStack(stock){
+  addStock(stock) {
     const control = this.form.get('stock') as FormArray;
-    console.log(stock);
     control.push(this.createStock(stock));
   }
 
-  removeStock({ group, index } : { group : FormGroup, index: number}){
-    // console.log(group, index);
+  removeStock({ group, index }: { group: FormGroup, index: number }) {
     const control = this.form.get('stock') as FormArray;
     control.removeAt(index);
   }
