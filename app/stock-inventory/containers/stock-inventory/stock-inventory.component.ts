b91ae5df9f2c98715684536c69b21f034c49bb8e
gengyudonga@gmail.com <gengyudonga@gmail.com>
2017-08-31 09:25:20 +0800

    Subscribing to the valueChanges Observable

--- app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
+++ app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
@@ -31,6 +31,10 @@ import { Product, Item } from '../../models/product.interface';
           (removed)="removeStock($event)">
         </stock-products>
 
+        <div class="stock-inventory__price">
+          Total: {{ total | currency: 'USD': true }}
+        </div>
+
         <div class="stock-inventory__buttons">
           <button 
             type="submit"
@@ -48,6 +52,8 @@ import { Product, Item } from '../../models/product.interface';
 export class StockInventoryComponent implements OnInit {
 
   products: Product[];
+
+  total: number;
 
   productMap: Map<number, Product>;
 
@@ -81,7 +87,20 @@ export class StockInventoryComponent implements OnInit {
         this.productMap = new Map<number, Product>(myMap);
         this.products = products;
         cart.forEach(item => this.addStock(item));
+
+        this.calculateTotal(this.form.get('stock').value);
+
+        this.form.get('stock')
+           .valueChanges.subscribe(value => this.calculateTotal(value));
       });
+
+  }
+
+  calculateTotal(value: Item[]){
+    const total = value.reduce((prev, next) => {
+      return prev + (next.quantity * this.productMap.get(next.product_id).price);
+    }, 0);
+    this.total = total;
 
   }
 
